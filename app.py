8f78a0e0c1bbe3cad7edabc05394a39fcc960e37
DobrevaI <iidobreva24a@gmail.com>
2026-01-29 09:32:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 
 ans=st.text_input("Do you like Python?")
-if ans==Yes || ans==yes || ans==yeah || ans==Yeah || ans==yep || ans==Yepe:
+if ans==Yes or ans==yes or ans==yeah or ans==Yeah or ans==yep or ans==Yepe:
   st.success("Amazing! :D")
-elif ans==no || ans==No || ans==nah || ans==Nah || ans==nope || ans==Nope:
+elif ans==no or ans==No or ans==nah or ans==Nah or ans==nope or ans==Nope:
   st.warning("You will like it! >:(")
 else:
   st.warning("I don't know what you said")
